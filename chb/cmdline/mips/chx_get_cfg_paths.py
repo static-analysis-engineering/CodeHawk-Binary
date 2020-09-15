2d862b79e67c4ba0658e204881628f17a7119477
Henny Sipma <hennysipma@mac.com>
2020-09-15 16:47:24 -0700

    add option to extract string constraints

--- chb/cmdline/mips/chx_get_cfg_paths.py
+++ chb/cmdline/mips/chx_get_cfg_paths.py
@@ -6,6 +6,7 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020      Henny Sipma
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -46,8 +47,12 @@ def parse():
                             action='store_true')
     parser.add_argument('--graph',help='produce a graphical representation using dot',
                             action='store_true')
-    parser.add_argument('--constraints',help='produce constraints',
+    parser.add_argument('--stringconstraints',help='output string constraints',
                             action='store_true')
+    parser.add_argument('--calls',help='output calls along the paths',
+                        action='store_true')
+    parser.add_argument('--verbose',help='output all info',action='store_true')
+    parser.add_argument('--maxtime',help='maximum search time',type=float)
     args  = parser.parse_args()
     return args
 
@@ -56,6 +61,35 @@ def terminate(msg):
     print(msg)
     print('*' * 80)
     exit(1)
+
+def get_string_constraints(paths):
+    sharedkonstraints = {}
+    allkonstraints = {}
+    for sink in paths:
+        if not paths[sink]: continue
+        pathconstraints = [ str(c) for c in paths[sink][0].get_constraints() if c ]
+        sharedkonstraints[sink] = set(pathconstraints[:])
+        allkonstraints[sink] = set([])
+        for path in paths[sink]:
+            pathconstraints = set([ str(c) for c in path.get_constraints() if c ])
+            sharedkonstraints[sink] &= pathconstraints
+            allkonstraints[sink] |= pathconstraints
+    return (sharedkonstraints,allkonstraints)
+
+def get_calls(paths):
+    sharedcalls = {}
+    allcalls = {}
+    for sink in paths:
+        if not paths[sink]: continue
+        calls = paths[sink][0].get_block_call_instruction_strings()
+        sharedcalls[sink] = set(calls[:])
+        allcalls[sink] = set(calls[:])
+        for path in paths[sink]:
+            calls = set(path.get_block_call_instruction_strings())
+            sharedcalls[sink] &= calls
+            allcalls[sink] |= calls
+    return (sharedcalls,allcalls)
+
 
 if __name__ == '__main__':
 
@@ -81,7 +115,7 @@ if __name__ == '__main__':
         faddr = args.function
 
     if not app.has_function(faddr):
-        terminate('No function found with address ' + args.faddr)
+        terminate('No function found with address ' + args.function)
 
     f = app.get_function(faddr)
 
@@ -106,7 +140,7 @@ if __name__ == '__main__':
     infeasiblepaths = []   # list of paths
     
     for sink in blocksinks:
-        cfgpaths[sink] = f.cfg.get_paths(sink)     # [ MIPSCfgPath ]
+        cfgpaths[sink] = f.cfg.get_paths(sink,maxtime=args.maxtime)     # [ MIPSCfgPath ]
 
     feasiblepaths = {}
     infeasiblepaths = 0
@@ -121,94 +155,29 @@ if __name__ == '__main__':
     feasiblepathcount = sum([ len(feasiblepaths[b]) for b in feasiblepaths ])
     pathcount = feasiblepathcount + infeasiblepaths
 
-    print('Feasible paths:   ' + str(feasiblepathcount).rjust(4))
-    print('Infeasible paths: ' + str(infeasiblepaths).rjust(4))
-    print('                  ' + ('-' * 4))
-    print('Total:            ' + str(pathcount).rjust(4))
-    print('\n\n')
-    
-    if args.constraints:
-        sharedconditions = {}
-        allconditions = {}
-        sharedcalls = {}
-        allcalls = {}
-        for sink in sorted(feasiblepaths):
-            blockconditions = feasiblepaths[sink][0].get_block_condition_strings()
-            blockcallinstrs = feasiblepaths[sink][0].get_block_call_instruction_strings()            
-            sharedconditions[sink] = set(blockconditions[:])
-            allconditions[sink] = set(blockconditions[:])
-            sharedcalls[sink] = set(blockcallinstrs[:])
-            allcalls[sink] = set(blockcallinstrs[:])
-            for path in feasiblepaths[sink]:
-                pathconditions = path.get_conditions()
-                blockconditions = path.get_block_condition_strings()
-                sharedconditions[sink] &= set(blockconditions[:])
-                allconditions[sink] |= set(blockconditions[:])
-                notcovered = []
-                print('')
-                print('=' * 80)
-                print('Constraints to reach:')
-                print('  ' + sink + ': ' + str(blocksinks[sink].get_annotation()))
-                for a in blocksinks[sink].get_call_arguments():
-                    if a.is_stack_address():
-                        print('    ' +  str(a))
-                print('=' * 80)
-                pathconstraints = path.get_constraints()
-                callinstrs = path.get_call_instructions()
-                for i,konstraint in enumerate(pathconstraints):
-                    block = path.path[i]
-                    if konstraint is None:
-                        if pathconditions[i] is None:
-                            continue
-                        else:
-                            notcovered.append((i,block,str(pathconditions[i])))
-                    else:
-                        print('  ' + str(konstraint))
-                if len(notcovered) > 0:
-                    print('-' * 80)
-                    print('Not yet covered: ' )
-                    for (i,block,x) in sorted(notcovered):
-                        print('  ' + str(i).rjust(4) + '  ' + block + ': ' + str(x))
-                if len(callinstrs) > 0:
-                    blockcallinstrs = path.get_block_call_instruction_strings()
-                    sharedcalls[sink] &= set(blockcallinstrs[:])
-                    allcalls[sink] |= set(blockcallinstrs[:])
-                    print('-' * 80)
-                    print('Calls: ')
-                    for i,c in enumerate(callinstrs):
-                        for instr in c:
-                            block = path.path[i]
-                            print('  ' + str(i).rjust(4) + '  ' + block + ': ' + str(instr.get_annotation()))
+    if args.verbose:
+        print('Feasible paths:   ' + str(feasiblepathcount).rjust(4))
+        print('Infeasible paths: ' + str(infeasiblepaths).rjust(4))
+        print('                  ' + ('-' * 4))
+        print('Total:            ' + str(pathcount).rjust(4))
+        print('\n\n')
 
-        print('\nShared conditions:')
-        print('-' * 80)
-        for sink in sharedconditions:
-            print('\nSink: ' + str(sink))
-            for c in sorted(sharedconditions[sink]):
-                print('  ' + c[0] + ':' + c[1])
+    if args.stringconstraints:
+        (sharedkonstraints,allkonstraints) = get_string_constraints(feasiblepaths)
+        print('\nShared constraints')
+        for sink in sharedkonstraints:
+            print('Sink: ' + str(sink))
+            for k in sharedkonstraints[sink]:
+                print('  ' + k)
 
-        print('\nNonshared conditions: ')
-        print('-' * 80)
-        for sink in allconditions:
-            print('\nSink: ' + str(sink))
-            for c in sorted(allconditions[sink]):
-                if c in sharedconditions[sink]: continue
-                print('  ' + c[0] + ':' + c[1])
-
-        print('\nShared calls:')
-        print('-' * 80)
+    if args.calls:
+        (sharedcalls,allcalls) = get_calls(feasiblepaths)
+        print('\nShared calls')
         for sink in sharedcalls:
-            print('\nSink: ' + str(sink))
+            print('Sink: ' + str(sink))
             for c in sorted(sharedcalls[sink]):
                 print('  ' + c[0] + ':' + c[1] + '  ' + c[2])
 
-        print('\nNonshared calls: ')
-        print('-' * 80)
-        for sink in allcalls:
-            print('\nSink: ' + str(sink))
-            for c in sorted(allcalls[sink]):
-                if c in sharedcalls[sink]: continue
-                print('  ' + c[0] + ':' + c[1] + '  ' + c[2])
 
     if args.graph:
         def getcolor(n):
@@ -243,8 +212,5 @@ if __name__ == '__main__':
         print('Restricted cfg for ' + filename + ' has been saved in '
                   + pdffilename)
         print('~' * 80)
-            
 
-                                                                 
-            
     
