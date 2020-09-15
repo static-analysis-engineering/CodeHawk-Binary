8d2c5109ddfe77da630fcd66d1d2cab88bc5d97e
Henny Sipma <hennysipma@mac.com>
2020-09-15 16:05:22 -0700

    add reverse edges to the callgraph

--- chb/app/Callgraph.py
+++ chb/app/Callgraph.py
@@ -5,6 +5,7 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020      Henny Sipma
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -32,7 +33,8 @@ class Callgraph(object):
     def __init__(self,app):
         self.app = app
         self.nodes = set([])
-        self.edges = {}
+        self.edges = {}            # faddr -> faddr/name -> int
+        self.revedges = {}         # faddr/name -> faddr -> int
         self._initialize()
 
     def get_name(self,n):
@@ -40,6 +42,14 @@ class Callgraph(object):
             return self.app.get_function_name(n)
         else:
             return n
+
+    def reverse_edges(self):
+        if len(self.revedges) == 0:
+            for src in self.edges:
+                for dst in self.edges[src]:
+                    self.revedges.setdefault(dst,{})
+                    self.revedges[dst].setdefault(src,0)
+                    self.revedges[dst][src] += 1
 
     def _initialize(self):
         calls = self.app.get_call_instructions()  # faddr -> instr
@@ -52,11 +62,21 @@ class Callgraph(object):
                     tgt = str(tgt)
                     self.nodes.add(tgt)
                     self.edges[faddr].setdefault(tgt,0)
-                    self.edges[faddr][tgt]
+                    self.edges[faddr][tgt] += 1
 
 
     def get_paths(self,src,dst):
         g = UG.DirectedGraph(self.nodes,self.edges)
         g.find_paths(src,dst)
         return g.paths
-            
+
+    def get_reverse_paths(self,src):
+        self.reverse_edges()
+        g = UG.DirectedGraph(self.nodes,self.revedges)
+        g.find_paths(src)
+        return g.paths
+
+    def get_reverse_callgraph(self):
+        self.reverse_edges()
+        return self.revedges
+
