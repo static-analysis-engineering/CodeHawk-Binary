80d771e838668c030a755738b29ca05a468b9d72
Henny Sipma <hennysipma@mac.com>
2021-04-05 22:36:09 -0700

    add some typing

--- chb/util/Config.py
+++ chb/util/Config.py
@@ -6,6 +6,7 @@
 #
 # Copyright (c) 2016-2020 Kestrel Technology LLC
 # Copyright (c) 2020      Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -16,7 +17,7 @@
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,71 +26,65 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # ------------------------------------------------------------------------------
+"""Contains the locations of various components used by the analyzer."""
 
 import os
 
+from typing import List
+
 localconfig = False
 
-if os.path.isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)),'ConfigLocal.py')):
+if os.path.isfile(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               "ConfigLocal.py")):
     import chb.util.ConfigLocal as ConfigLocal
     localconfig = True
 
+
 class Config():
 
-    def __init__(self):
+    def __init__(self) -> None:
         # platform settings
-        if os.uname()[0] == 'Linux': self.platform = 'linux'
-        elif os.uname()[0] == 'Darwin': self.platform = 'macOS'
+        if os.uname()[0] == 'Linux':
+            self.platform = 'linux'
+        elif os.uname()[0] == 'Darwin':
+            self.platform = 'macOS'
 
         # general settings
         self.utildir = os.path.dirname(os.path.abspath(__file__))
         self.chbdir = os.path.dirname(self.utildir)
         self.rootdir = os.path.dirname(self.chbdir)
-        self.testsdir = os.path.join(self.rootdir,'tests')
-        self.projects = os.path.join(self.testsdir,'projects.json')
-        self.bindir = os.path.join(self.chbdir,'bin')
-        self.binariesdir = os.path.join(self.bindir,'binaries')
-        self.summariesdir = os.path.join(self.chbdir,'summaries')
-        self.summaries = os.path.join(self.summariesdir,'bchsummaries.jar')
-        
+        self.testsdir = os.path.join(self.rootdir, "tests")
+        self.projects = os.path.join(self.testsdir, "projects.json")
+        self.bindir = os.path.join(self.chbdir, "bin")
+        self.binariesdir = os.path.join(self.bindir, "binaries")
+        self.summariesdir = os.path.join(self.chbdir, "summaries")
+        self.summaries = os.path.join(self.summariesdir, "bchsummaries.jar")
+
         # analyzer location
         if self.platform == 'linux':
-            self.linuxdir = os.path.join(self.binariesdir,'linux')
-            self.chx86_analyze = os.path.join(self.linuxdir,'chx86_analyze')
+            self.linuxdir = os.path.join(self.binariesdir, "linux")
+            self.chx86_analyze = os.path.join(self.linuxdir, "chx86_analyze")
 
-        elif self.platform == 'macOS':
-            self,macOSdir = os.path.join(self.binariesdir,'macOS')
-            self.chx86_analyze = os.path.join(self.macOSdir,'chx86_analyze')
+        elif self.platform == "macOS":
+            self.macOSdir = os.path.join(self.binariesdir, "macOS")
+            self.chx86_analyze = os.path.join(self.macOSdir, "chx86_analyze")
 
-        # optional: set architecture-specific analysis targets to provide
-        #   shortcuts to filenames, and to maintain meta data on analysis targets
-        #   (see example in ConfigLocal.template), and description in fileutil.py
-        self.analysistargettable = {}
-        self.atsc_separator = ':'  # analysis_target_shortcut_name separator
-
-        # reference features
-        self.reference_features = {}
-            
         # personalization
-        if localconfig: ConfigLocal.getLocals(self)
+        if localconfig:
+            ConfigLocal.getLocals(self)
 
-    def __str__(self):
-        lines = []
-        analyzerfound = ' (found)' if os.path.isfile(self.chx86_analyze) else ' (not found)'
-        summariesfound = ' (found)' if os.path.isfile(self.summaries) else  ' (not found)'
-        lines.append('Analyzer configuration:')
-        lines.append('-----------------------')
-        lines.append('  analyzer : ' + self.chx86_analyze + analyzerfound)
-        lines.append('  summaries: ' + self.summaries + summariesfound)
-        if len(self.analysistargettable) > 0:
-            lines.append('\nAnalysis target index files:')
-            lines.append('----------------------------')
-            for arch in self.analysistargettable:
-                lines.append('  ' + arch + ':')
-                archtargets = self.analysistargettable[arch]
-                for ati_key in archtargets:
-                    lines.append('    '  + ati_key + ': ' + archtargets[ati_key])
+    def __str__(self) -> str:
+        lines: List[str] = []
+        analyzerfound = (
+            " (found)" if os.path.isfile(self.chx86_analyze) else " (not found)")
+        summariesfound = (
+            " (found)" if os.path.isfile(self.summaries) else " (not found)")
+        lines.append("Analyzer configuration:")
+        lines.append("-----------------------")
+        lines.append("  analyzer : " + self.chx86_analyze + analyzerfound)
+        lines.append("  summaries: " + self.summaries + summariesfound)
         return '\n'.join(lines)
+
 
 if __name__ == '__main__':
 
--- chb/util/StringIndexedTable.py
+++ chb/util/StringIndexedTable.py
@@ -1,10 +1,12 @@
 # ------------------------------------------------------------------------------
-# Access to the CodeHawk Binary Analyzer Analysis Results
+# CodeHawk Binary Analyzer
 # Author: Henny Sipma
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2017-2020 Kestrel Technology LLC
+# Copyright (c) 2020      Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -15,7 +17,7 @@
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,67 +29,77 @@
 
 import xml.etree.ElementTree as ET
 
+import chb.util.fileutil as UF
 import chb.util.IndexedTable as IT
 
-def has_control_characters(s):
+from typing import Dict, List, Optional, Tuple
+
+def has_control_characters(s: str) -> bool:
     for c in s:
         if ord(c) < 32 or ord(c) > 126:
             return True
-
     else:
         return False
 
-def byte_to_string(b): return '{:02x}'.format(b)
 
-def value_from_hex(s): return int(s,16)
+def byte_to_string(b: int) -> str:
+    return '{:02x}'.format(b)
 
-def hexstring(s):
+
+def value_from_hex(s: str) -> int:
+    return int(s,16)
+
+
+def hexstring(s: str) -> str:
     result = ''
     for c in s:
         result += byte_to_string(ord(c))
     return result
 
-def dehexstring(h):
+
+def dehexstring(h: str) -> str:
     h = h[:]
     result = ''
     try:
-        for i in range(len(h)/2):
-            result += chr(int(h[:2],16))
+        for i in range(len(h) // 2):
+            result += chr(int(h[:2], 16))
             h = h[2:]
         return result
     except:
         print('Error in dehexing string: ' + h)
         exit(1)
 
-def decode(ishex,h):
+
+def decode(ishex: bool, h: str) -> str:
     if ishex:
         return dehexstring(h)
     else:
         return h
 
-def encode(s):
+
+def encode(s: str) -> Tuple[bool, str]:
     if has_control_characters(s):
-        return (True,hexstring(s))
+        return (True, hexstring(s))
     else:
-        return (False,s)
+        return (False, s)
 
 
-class StringIndexedTable(object):
+class StringIndexedTable:
 
-    def __init__(self,name):
+    def __init__(self, name: str) -> None:
         self.name = name
-        self.stringtable= {}      # string -> index
-        self.indextable = {}      # index -> string
+        self.stringtable: Dict[str, int] = {}      # string -> index
+        self.indextable: Dict[int, str] = {}      # index -> string
         self.next = 1
 
-    def reset(self):
+    def reset(self) -> None:
         self.stringtable = {}
         self.indextable = {}
         self.next = 1
 
-    def add(self,s):
+    def add(self, s: str) -> int:
         if s is None:
-            raise  IT.IndexedTableError(self.name + ': Attempt to index None')
+            raise IT.IndexedTableError(self.name + ': Attempt to index None')
         if s in self.stringtable:
             return self.stringtable[s]
         else:
@@ -97,42 +109,62 @@ class StringIndexedTable(object):
             self.next += 1
             return index
 
-    def size(self): return (self.next - 1)
+    def size(self) -> int:
+        return (self.next - 1)
 
-    def values(self): return sorted(self.stringtable.keys())
+    def values(self) -> List[str]:
+        return sorted(self.stringtable.keys())
 
-    def retrieve(self,index):
+    def retrieve(self, index: int) -> str:
         if index in self.indextable:
             return self.indextable[index]
         else:
-            msg = ('Unable to retrieve item ' + str(index) + ' from table ' + self.name
-                      + ' (size: ' + str(self.size()) + ')')
-            raise IT.IndexedTableError(msg + '\n' + self.name + ', size: ' + str(self.size()))
+            msg = (
+                'Unable to retrieve item '
+                + str(index)
+                + ' from table '
+                + self.name
+                + ' (size: '
+                + str(self.size())
+                + ')')
+            raise IT.IndexedTableError(
+                msg
+                + '\n'
+                + self.name
+                + ', size: '
+                + str(self.size()))
 
-    def read_xml(self,node):
+    def read_xml(self, node: Optional[ET.Element]) -> None:
         if node is None:
             print('Xml node not present in string table')
             raise IT.IndexedTableError('Xml node not present in string table')
         for snode in node.findall('n'):
-            index = int(snode.get('ix'))
+            ix = snode.get("ix")
+            if ix is None :
+                raise UF.CHBError("StringIndexTable: index is missing")
+            index = int(ix)
             ishex = snode.get('hex','no') == 'yes'
-            s = decode(ishex,snode.get('v'))
+            v = snode.get("v")
+            if v is None:
+                raise UF.CHBError(
+                    "StringIndexTable: value is missing for index " + str(index))
+            s = decode(ishex, v)
             self.stringtable[s] = index
             self.indextable[index] = s
             if index >= self.next:
                 self.next = index + 1
 
-    def write_xml(self,node):
+    def write_xml(self, node: ET.Element) -> None:
         for index in sorted(self.indextable):
             s = self.indextable[index]
-            (ishex,sencoded) = encode(s)
+            (ishex, sencoded) = encode(s)
             snode = ET.Element('n')
-            snode.set('v',sencoded)
-            snode.set('ix',str(index))
+            snode.set('v', sencoded)
+            snode.set('ix', str(index))
             node.append(snode)
 
-    def __str__(self):
-        lines = []
+    def __str__(self) -> str:
+        lines: List[str] = []
         lines.append('\nstring-table')
         for ix in sorted(self.indextable):
             lines.append(str(ix).rjust(4) + ' ' + str(self.indextable[ix]))
