b1a9aae663734d38b9617272f948cccb388fbf6a
Henny Sipma <hennysipma@mac.com>
2021-04-30 11:29:41 -0700

    add typing to PE file format support code

--- chb/peformat/PEImportDirectoryNameEntry.py
+++ chb/peformat/PEImportDirectoryNameEntry.py
@@ -1,10 +1,12 @@
 # ------------------------------------------------------------------------------
-# Access to the CodeHawk Binary Analyzer Analysis Results
+# CodeHawk Binary Analyzer
 # Author: Henny Sipma
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020      Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -15,7 +17,7 @@
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,37 +26,86 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # ------------------------------------------------------------------------------
+import xml.etree.ElementTree as ET
+
+from typing import Dict, List, TYPE_CHECKING
 
 import chb.util.fileutil as UF
 
-class PEImportDirectoryNameEntry():
-    '''Represents a single entry in an import table'''
+if TYPE_CHECKING:
+    import chb.peformat.PEImportDirectoryEntry
 
-    def __init__(self,peimporttable,xnode):
+
+class PEImportDirectoryNameEntry():
+    """epresents a single entry in an import table"""
+
+    def __init__(
+            self,
+            peimporttable: "chb.peformat.PEImportDirectoryEntry.PEImportDirectoryEntry",
+            xnode: ET.Element) -> None:
         self.peimporttable = peimporttable
         self.xnode = xnode
-        self.address = self.xnode.get('bound-address')
-        self.hint = self.xnode.get('hint')
-        self.name = self.xnode.get('name')
-        self.rva = self.xnode.get('rva')
 
-    def has_summary(self):
+    @property
+    def address(self) -> str:
+        xaddr = self.xnode.get("bound-address")
+        if xaddr:
+            return xaddr
+        else:
+            raise UF.CHBError(
+                "No bound-address found in PEImportDirectoryNameEntry")
+
+    @property
+    def hint(self) -> str:
+        xhint = self.xnode.get("hint")
+        if xhint:
+            return xhint
+        else:
+            raise UF.CHBError("No hint found in PEImportDirectoryNameEntry")
+
+    @property
+    def name(self) -> str:
+        xname = self.xnode.get("name")
+        if xname:
+            return xname
+        else:
+            raise UF.CHBError("No name found in PEImportDirectoryNameEntry")
+
+    @property
+    def rva(self) -> str:
+        xrva = self.xnode.get("rva")
+        if xrva:
+            return xrva
+        else:
+            raise UF.CHBError("No rva found in PEImportDirectoryNameEntry")
+
+    @property
+    def has_summary(self) -> bool:
         return self.peimporttable.has_summary(self.name)
 
-    def as_dictionary(self):
+    @property
+    def as_dictionary(self) -> Dict[str, str]:
         result = {
             "name": self.name,
             "hint": self.hint,
             "address": self.address,
             "rva": self.rva,
-            "summary": "Y" if self.has_summary() else "N"
+            "summary": "Y" if self.has_summary else "N"
             }
         return result
 
-    def __str__(self):
-        summary = ' '
-        if self.has_summary(): summary = 'Y'
+    def __str__(self) -> str:
+        summary = " "
+        if self.has_summary:
+            summary = "Y"
         hint = self.hint
-        if hint is None: hint = ' '
-        return ((' ' * 3) + hint.rjust(4) + '  ' + self.address +
-                '  ' + summary + '  ' + self.name)
+        if hint is None:
+            hint = " "
+        return ((" " * 3)
+                + hint.rjust(4)
+                + "  "
+                + self.address
+                + "  "
+                + summary
+                + "  "
+                + self.name)
--- chb/peformat/PESectionHeader.py
+++ chb/peformat/PESectionHeader.py
@@ -26,8 +26,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # ------------------------------------------------------------------------------
+import xml.etree.ElementTree as ET
+
+from typing import Any, Dict, List, TYPE_CHECKING
 
 import chb.util.fileutil as UF
+
+if TYPE_CHECKING:
+    import chb.peformat.PEHeader
 
 section_header_attributes = [
     "characteristics",
@@ -40,65 +46,159 @@ section_header_attributes = [
     ]
 
 
-class PESectionHeader():
+class PESectionHeader:
     '''Represents the header data of a single section in the executable.'''
 
-    def __init__(self,peheader,xnode):
+    def __init__(
+            self,
+            peheader: "chb.peformat.PEHeader.PEHeader",
+            xnode: ET.Element) -> None:
         self.peheader = peheader
         self.xnode = xnode
-        self.name = self.xnode.get('name')
-        self.characteristics = self.xnode.get('characteristics')
-        self.numberoflinenumbers = self.xnode.get('number-of-line-numbers')
-        self.numberofrelocations = self.xnode.get('number-of-relocations')
-        self.pointertorawdata = self.xnode.get('pointer-to-raw-data')
-        self.pointertorelocations = self.xnode.get('pointer-to-relocations')
-        self.sizeofrawdata = self.xnode.get('size-of-raw-data')
-        self.va = self.xnode.get('virtual-address')
-        self.virtualsize = self.xnode.get('virtual-size')
 
-    def str_characteristics(self):
-        lines = []
-        for x in self.xnode.find('section-charxs').findall('charx'):
-            lines.append((' ' * 3) + x.get('name'))
+    @property
+    def name(self) -> str:
+        xname = self.xnode.get("name")
+        if xname:
+            return xname
+        else:
+            raise UF.CHBError("Name is missing from PESectionHeader")
+
+    @property
+    def characteristics(self) -> str:
+        xchars = self.xnode.get("characteristics")
+        if xchars:
+            return xchars
+        else:
+            raise UF.CHBError("Characteristics are missing from PESectionHeader")
+
+    @property
+    def number_of_line_numbers(self) -> str:
+        xlines = self.xnode.get("number-of-line-numbers")
+        if xlines:
+            return xlines
+        else:
+            raise UF.CHBError(
+                "Number-of-line-numbers is missing from PESectionHeader")
+
+    @property
+    def number_of_relocations(self) -> str:
+        xreloc = self.xnode.get("number-of-relocations")
+        if xreloc:
+            return xreloc
+        else:
+            raise UF.CHBError(
+                "Number-of-relocations is missing from PESectionHeader")
+
+    @property
+    def pointer_to_raw_data(self) -> str:
+        xptr = self.xnode.get("pointer-to-raw-data")
+        if xptr:
+            return xptr
+        else:
+            raise UF.CHBError(
+                "Pointer-to-raw-data is missing from PESectionHeader")
+
+    @property
+    def pointer_to_relocations(self) -> str:
+        xptr = self.xnode.get("pointer-to-relocations")
+        if xptr:
+            return xptr
+        else:
+            raise UF.CHBError(
+                "Pointer to relocations is missing from PESectionHeader")
+
+    @property
+    def size_of_raw_data(self) -> str:
+        xsize = self.xnode.get("size-of-raw-data")
+        if xsize:
+            return xsize
+        else:
+            raise UF.CHBError(
+                "Size of raw data is missing from PESectionHeader")
+
+    @property
+    def virtual_address(self) -> str:
+        xva = self.xnode.get("virtual-address")
+        if xva:
+            return xva
+        else:
+            raise UF.CHBError("Virtual address is missing from PESectionHeader")
+
+    @property
+    def virtual_size(self) -> str:
+        xsize = self.xnode.get("virtual-size")
+        if xsize:
+            return xsize
+        else:
+            raise UF.CHBError("Virtual size is missing from PESectionHeader")
+
+    @property
+    def characteristics_text(self) -> List[str]:
+        lines: List[str] = []
+        xcharx = self.xnode.find("section-charxs")
+        if xcharx:
+            for x in xcharx.findall("charx"):
+                xname = x.get("name")
+                if xname:
+                    lines.append((" " * 3) + xname)
+                else:
+                    raise UF.CHBError("Name missing from characteristics text")
+        else:
+            raise UF.CHBError(
+                "Section-characteristics text missing from PESectionHeader")
         return lines
 
-    def is_executable(self):
-        for x in self.xnode.find('section-charxs').findall('charx'):
-            if x.get('name') == 'IMAGE_SCN_MEM_EXECUTE': return True
+    @property
+    def is_executable(self) -> bool:
+        xcharx = self.xnode.find("section-charxs")
+        if xcharx:
+            for x in xcharx.findall("charx"):
+                xname = x.get("name")
+                if xname:
+                    if xname == "IMAGE_SCN_MEM_EXECUTE":
+                        return True
+                else:
+                    raise UF.CHBError("Name missing from characteristics text")
+            else:
+                return False
         else:
-            return False
+            raise UF.CHBError(
+                "Section-characteristics text missing from PESectionHeader")
 
-    def as_dictionary(self):
-        result = {}
-        result['name'] = self.name
-        localetable = UF.get_locale_tables(tables=[ ("PE","pesectionheader") ])
+    def as_dictionary(self) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+        result["name"] = self.name
+        localetable = UF.get_locale_tables(tables=[("PE", "pesectionheader")])
         for p in section_header_attributes:
-            propertyvalue = self.xnode.get(p,'0x0')
+            propertyvalue = self.xnode.get(p, "0x0")
             result[p] = {}
-            result[p]['value'] = propertyvalue
-            result[p]['heading'] = localetable['pesectionheader'][p]
-        result['section-characterists'] = sectionxs = {}
-        sectionxs['value'] = ','.join(self.str_characteristics())
-        sectionxs['heading'] = 'Section characteristics'
+            result[p]["value"] = propertyvalue
+            result[p]["heading"] = localetable["pesectionheader"][p]
+        result["section-characterists"] = sectionxs = {}
+        sectionxs["value"] = ",".join(self.characteristics_text)
+        sectionxs["heading"] = "Section characteristics"
         return result
 
-    def __str__(self):
-        lines = []
-        def addline(tag,value):
-            lines.append((' ' * 3) + tag.ljust(32) + ': ' + str(value))
-        lines.append('-' * 60)
-        lines.append('Section header for ' + self.name)
-        lines.append('-' * 60)
-        addline('Name', self.name)
-        addline('Virtual size', self.virtualsize)
-        addline('Virtual address', self.va)
-        addline('Size of raw data', self.sizeofrawdata)
-        addline('Pointer to raw data', self.pointertorawdata)
-        addline('Pointer to relocations', self.pointertorelocations)
-        addline('Number of line numbers', self.numberoflinenumbers)
-        addline('Number of relocations', self.numberofrelocations)
-        addline('Characteristics', self.characteristics)
-        lines.append(' ')
-        lines.extend(self.str_characteristics())
-        lines.append('-' * 60)
-        return '\n'.join(lines)
+    def __str__(self) -> str:
+        lines: List[str] = []
+
+        def addline(tag: str, value: str) -> None:
+            lines.append((" " * 3) + tag.ljust(32) + ": " + str(value))
+
+        lines.append("-" * 60)
+        lines.append("Section header for " + self.name)
+        lines.append("-" * 60)
+        addline("Name", self.name)
+        addline("Virtual size", self.virtual_size)
+        addline("Virtual address", self.virtual_address)
+        addline("Size of raw data", self.size_of_raw_data)
+        addline("Pointer to raw data", self.pointer_to_raw_data)
+        addline("Pointer to relocations", self.pointer_to_relocations)
+        addline("Number of line numbers", self.number_of_line_numbers)
+        addline("Number of relocations", self.number_of_relocations)
+        addline("Characteristics", self.characteristics)
+        lines.append(" ")
+        lines.extend(self.characteristics_text)
+        lines.append("-" * 60)
+        return "\n".join(lines)
