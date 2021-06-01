80fcdcbbafbf4774046f5f293d62950d22c5c3ee
Henny Sipma <hennysipma@mac.com>
2021-05-31 22:43:32 -0700

    x86: move opcodes

---
+++ chb/x86/opcodes/X86Lea.py
@@ -0,0 +1,111 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020      Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import cast, List, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.invariants.XVariable import XVariable
+from chb.invariants.XXpr import XXpr
+
+import chb.simulation.SimUtil as SU
+import chb.simulation.SimValue as SV
+
+from chb.util.IndexedTable import IndexedTableValue
+import chb.util.fileutil as UF
+
+from chb.x86.X86DictionaryRecord import x86registry
+from chb.x86.X86Opcode import X86Opcode, simplify_result
+from chb.x86.X86Operand import X86Operand
+
+if TYPE_CHECKING:
+    from chb.x86.X86Dictionary import X86Dictionary
+    from chb.x86.simulation.X86SimulationState import X86SimulationState    
+
+
+@x86registry.register_tag("lea", X86Opcode)
+class X86Lea(X86Opcode):
+    """LEA dst, src
+
+    args[0]: index of dst in x86dictionary
+    args[1]: index of src in x86dictionary
+    """
+
+    def __init__(
+            self,
+            x86d: "X86Dictionary",
+            ixval: IndexedTableValue) -> None:
+        X86Opcode.__init__(self, x86d, ixval)
+
+    @property
+    def src_operand(self) -> X86Operand:
+        return self.x86d.get_operand(self.args[1])
+
+    @property
+    def dst_operand(self) -> X86Operand:
+        return self.x86d.get_operand(self.args[0])
+
+    def get_operands(self) -> List[X86Operand]:
+        return [self.dst_operand, self.src_operand]
+
+    def get_opcode_operations(self) -> List[str]:
+        src = self.src_operand
+        dst = self.dst_operand
+        return [dst.to_operand_string() + ' = ' + src.to_address_string()]
+
+    # xdata: [ "a:vxx": lhs, rhs, rhs-simplified ]
+    def get_annotation(self, xdata: InstrXData) -> str:
+        """data format: a:vxxx .
+        
+        vars[0]: lhs
+        xprs[0]: rhs
+        xprs[1]: rhs simplified
+        """
+
+        lhs = str(xdata.vars[0])
+        rhs = xdata.xprs[0]
+        rrhs = xdata.xprs[1]
+        xrhs = simplify_result(xdata.args[1],xdata.args[2], rhs, rrhs)
+        return lhs + ' = ' + xrhs
+
+    def get_lhs(self, xdata: InstrXData) -> List[XVariable]:
+        return [xdata.vars[0]]
+
+    def get_rhs(self, xdata: InstrXData) -> List[XXpr]:
+        return [xdata.xprs[1]]
+
+    # --------------------------------------------------------------------------
+    # Computes the effective address of the second operand (the source operand)
+    # and stores it in the first operand (destination operand).
+    #
+    # Flags affected: None
+    # --------------------------------------------------------------------------
+    def simulate(self, iaddr: str, simstate: "X86SimulationState") -> None:
+        addressval = simstate.get_address_val(iaddr, self.src_operand)
+        simstate.set(iaddr, self.dst_operand, addressval)
