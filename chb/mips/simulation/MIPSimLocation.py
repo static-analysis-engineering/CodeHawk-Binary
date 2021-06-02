04af6c2aeef9c2ee6a5977d9daf1b6ee3d10c2cc
Henny Sipma <hennysipma@mac.com>
2021-06-01 21:46:45 -0700

    move mips simulation classes

--- chb/mips/simulation/MIPSimLocation.py
+++ chb/mips/simulation/MIPSimLocation.py
@@ -1,10 +1,11 @@
 # ------------------------------------------------------------------------------
-# Access to the CodeHawk Binary Analyzer Analysis Results
+# CodeHawk Binary Analyzer
 # Author: Henny Sipma
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
 # Copyright (c) 2020 Henny Sipma
+# Copyright (c) 2021 Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -15,7 +16,7 @@
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,47 +26,32 @@
 # SOFTWARE.
 # ------------------------------------------------------------------------------
 
+from chb.simulation.SimLocation import SimRegister, SimMemoryLocation
+from chb.simulation.SimSymbolicValue import SimAddress
+
 import chb.util.fileutil as UF
 
-class MIPSimLocation(object):
 
-    def __init__(self): pass
+class MIPSimRegister(SimRegister):
 
-    def is_register(self): return False
-    def is_memory_location(self): return False
+    def __init__(self, reg: str) -> None:
+        SimRegister.__init__(self, reg)
 
-    def __str__(self): return 'mips-location'
 
-class MIPSimRegister(MIPSimLocation):
+class MIPSimMemoryLocation(SimMemoryLocation):
 
-    def __init__(self,reg):
-        self.reg = reg
+    def __init__(self, simaddress: SimAddress) -> None:
+        SimMemoryLocation.__init__(self, simaddress)
 
-    def is_register(self): return True
+    def is_base_location(self) -> bool:
+        return self.simaddress.is_base_address()
 
-    def __str__(self): return self.reg
-
-class MIPSimMemoryLocation(MIPSimLocation):
-
-    def __init__(self,address):
-        self.address = address  # SimAddress
-
-    def is_memory_location(self): return True
-
-    def is_stack_location(self): return self.address.is_stack_address()
-
-    def is_global_location(self): return self.address.is_global_address()
-
-    def is_base_location(self): return self.address.is_base_address()
-
-    def get_address(self): return self.address
-
-    def __str__(self):
-        if self.is_stack_location():
-            return 'stack[' + str(self.get_address().get_offset().to_signed_int()) + ']'
-        elif self.is_global_location():
-            return 'global[' + str(self.get_address().get_offset()) + ']'
+    def __str__(self) -> str:
+        if self.is_stack():
+            return "stack[" + str(self.simaddress.offsetvalue) + "]"
+        elif self.is_global():
+            return "global[" + str(self.simaddress.offsetvalue) + "]"
         elif self.is_base_location():
-            return self.address.get_base() + '[' + str(self.get_address().get_offset()) + ']'
+            return self.simaddress.base + "[" + str(self.simaddress.offsetvalue) + "]"
         else:
-            return str('loc@' + str(self.address))
+            return str("loc@" + str(self.simaddress))
