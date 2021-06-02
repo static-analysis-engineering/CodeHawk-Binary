795fb7882ffb1fb0b1760c86bc9d891f2c50412c
Henny Sipma <hennysipma@mac.com>
2021-06-02 13:13:16 -0700

    arm: opcode classes

---
+++ chb/arm/opcodes/ARMBitwiseOr.py
@@ -0,0 +1,84 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import List, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.arm.ARMDictionaryRecord import armregistry
+from chb.arm.ARMOpcode import ARMOpcode, simplify_result
+from chb.arm.ARMOperand import ARMOperand
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    import chb.arm.ARMDictionary
+
+
+@armregistry.register_tag("ORR", ARMOpcode)
+class ARMBitwiseOr(ARMOpcode):
+    """Bitwise and instruction (register, register-shifted, and immediate)
+
+    ORR{S}<c> <Rd>, <Rn>{, <shift>}
+    ORR{S}<c> <Rd>, <Rn>, <Rm>, <type> <Rs>
+
+    tags[1]: <c>
+    args[0]: {S}
+    args[1]: index of op1 in armdictionary
+    args[2]: index of op2 in armdictionary
+    args[3]: index of op3 in armdictionary
+    args[4]: is-wide (thumb)
+    """
+
+    def __init__(
+            self,
+            d: "chb.arm.ARMDictionary.ARMDictionary",
+            ixval: IndexedTableValue) -> None:
+        ARMOpcode.__init__(self, d, ixval)
+        self.check_key(2, 5, "BitwiseOr")
+
+    @property
+    def operands(self) -> List[ARMOperand]:
+        return [self.armd.get_arm_operand(i) for i in self.args[1: -1]]
+
+    def get_annotation(self, xdata: InstrXData) -> str:
+        """xdata format: a:vxxxx .
+
+        vars[0]: lhs
+        xprs[0]: rhs1
+        xprs[1]: rhs2
+        xprs[3]: rhs1 | rhs2 (syntactic)
+        xprs[4]: rhs1 | rhs2 (simplified)
+        """
+
+        lhs = str(xdata.vars[0])
+        result = xdata.xprs[2]
+        rresult = xdata.xprs[3]
+        xresult = simplify_result(xdata.args[3], xdata.args[4], result, rresult)
+        return lhs + " := " + xresult
---
+++ chb/arm/opcodes/ARMBranchExchange.py
@@ -0,0 +1,72 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import List, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.arm.ARMDictionaryRecord import armregistry
+from chb.arm.ARMOpcode import ARMOpcode, simplify_result
+from chb.arm.ARMOperand import ARMOperand
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    import chb.arm.ARMDictionary
+
+
+@armregistry.register_tag("BX", ARMOpcode)
+class ARMBranchExchange(ARMOpcode):
+    """Branch to an address and instruction set specified by a register.
+
+    tags[1]: <c>
+    args[0]: index of target operand in armdictionary
+    """
+
+    def __init__(
+            self,
+            d: "chb.arm.ARMDictionary.ARMDictionary",
+            ixval: IndexedTableValue) -> None:
+        ARMOpcode.__init__(self, d, ixval)
+        self.check_key(2, 1, "BranchExchange")
+
+    def get_operands(self) -> List[ARMOperand]:
+        return [self.armd.get_arm_operand(self.args[0])]
+
+    def get_annotation(self, xdata: InstrXData) -> str:
+        """xdata format: a:x .
+
+        xprs[0]: target operand
+        """
+
+        tgt = str(xdata.xprs[0])
+        if tgt == "LR":
+            return "return"
+        else:
+            return "goto " + tgt
