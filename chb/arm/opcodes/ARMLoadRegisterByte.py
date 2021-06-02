644cb97f116b57e59e59b271c8d3b6c06c17420e
Henny Sipma <hennysipma@mac.com>
2021-06-02 13:14:06 -0700

    arm: opcode classes

---
+++ chb/arm/opcodes/ARMLoadRegisterByte.py
@@ -0,0 +1,78 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import List, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.arm.ARMDictionaryRecord import armregistry
+from chb.arm.ARMOpcode import ARMOpcode, simplify_result
+from chb.arm.ARMOperand import ARMOperand
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    import chb.arm.ARMDictionary
+
+
+@armregistry.register_tag("LDRB", ARMOpcode)
+class ARMLoadRegisterByte(ARMOpcode):
+    """Loads a byte from memory, zero-extends it to 32 bits, and writes it to a register.
+
+    LDRB<c> <Rt>, [<base>, <offset>]
+
+    tags[0]: <c>
+    args[0]: index of destination operand in armdictionary
+    args[1]: index of base register in armdictionary
+    args[2]: index of memory location in armdictionary
+    args[3]: is-wide (thumb)
+    """
+
+    def __init__(
+            self,
+            d: "chb.arm.ARMDictionary.ARMDictionary",
+            ixval: IndexedTableValue) -> None:
+        ARMOpcode.__init__(self, d, ixval)
+        self.check_key(2, 4, "LoadRegisterByte")
+
+    @property
+    def operands(self) -> List[ARMOperand]:
+        return [self.armd.get_arm_operand(i) for i in self.args[:-1]]
+
+    def get_annotation(self, xdata: InstrXData) -> str:
+        """xdata format: a:vxx .
+
+        vars[0]: lhs
+        xprs[0]: value in memory location
+        xprs[1]: value in memory location (simplified)
+        """
+
+        lhs = str(xdata.vars[0])
+        rhs = str(xdata.xprs[1])
+        return lhs + " := " + rhs
