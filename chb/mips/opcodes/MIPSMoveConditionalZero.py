49cd375b83fd7bd52eb905ed5088b3d88acb5d37
Henny Sipma <hennysipma@mac.com>
2021-06-05 14:02:41 -0700

    mips: move opcodes out

---
+++ chb/mips/opcodes/MIPSMoveConditionalZero.py
@@ -0,0 +1,128 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020-2021 Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import cast, List, Sequence, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.invariants.XVariable import XVariable
+from chb.invariants.XXpr import XXpr
+
+from chb.mips.MIPSDictionaryRecord import mipsregistry
+from chb.mips.MIPSOpcode import MIPSOpcode, simplify_result
+from chb.mips.MIPSOperand import MIPSOperand
+
+import chb.simulation.SimSymbolicValue as SSV
+import chb.simulation.SimUtil as SU
+import chb.simulation.SimValue as SV
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.mips.MIPSDictionary import MIPSDictionary
+    from chb.mips.simulation.MIPSimulationState import MIPSimulationState
+
+
+@mipsregistry.register_tag("movz", MIPSOpcode)
+class MIPSMoveConditionalZero(MIPSOpcode):
+    """MOVZ rd, rs, rt
+
+    Move Conditional on Zero.
+    Conditionally move a GPR after testing a GPR value
+
+    args[0]: index of rd in mips dictionary
+    args[1]: index of rs in mips dictionary
+    args[2]: index of rt in mips dictionary
+    """
+
+    def __init__(
+            self,
+            mipsd: "MIPSDictionary",
+            ixval: IndexedTableValue) -> None:
+        MIPSOpcode.__init__(self, mipsd, ixval)
+
+    @property
+    def operands(self) -> Sequence[MIPSOperand]:
+        return [self.mipsd.mips_operand(i) for i in self.args]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        """data format a:vxxxx
+
+        vars[0]: lhs
+        xprs[0]: rhs
+        xprs[1]: testxpr
+        xprs[2]: condition (syntactic)
+        xprs[3]: condition (simplified)
+        """
+
+        lhs = str(xdata.vars[0])
+        rhs = str(xdata.xprs[0])
+        cond = xdata.xprs[1]
+        ccond = xdata.xprs[2]
+        xcond = simplify_result(xdata.args[2], xdata.args[3], cond, ccond)
+        return 'if ' + xcond + ' then ' + lhs + ' := ' + rhs
+
+    @property
+    def src_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[1])
+
+    @property
+    def test_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[2])
+
+    @property
+    def dst_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[0])
+
+    # --------------------------------------------------------------------------
+    # Operation:
+    #    if GPR[rt] = 0 then
+    #       GPR[rd] <- GPR[rs]
+    #    endif
+    # ---------------------------------------------------------------------------
+    def simulate(self, iaddr: str, simstate: "MIPSimulationState") -> str:
+        dstop = self.dst_operand
+        srcop = self.src_operand
+        testop = self.test_operand
+        srcval = simstate.get_rhs(iaddr, srcop)
+        testval = simstate.get_rhs(iaddr, testop)
+        if testval.is_literal and testval.is_defined:
+            testval = cast(SV.SimLiteralValue, testval)
+            if testval.value == 0:
+                lhs = simstate.set(iaddr, dstop, srcval)
+                simstate.increment_program_counter()
+                return SU.simassign(iaddr, simstate, lhs, srcval)
+            else:
+                simstate.increment_program_counter()
+                return 'nop'
+        else:
+            simstate.increment_program_counter()
+            return '?'
---
+++ chb/mips/opcodes/MIPSMoveWordFromHighHalfCoprocessor2.py
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020-2021 Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import cast, List, Sequence, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.invariants.XXpr import XXpr
+
+from chb.mips.MIPSDictionaryRecord import mipsregistry
+from chb.mips.MIPSOpcode import MIPSOpcode, simplify_result
+from chb.mips.MIPSOperand import MIPSOperand
+
+import chb.simulation.SimSymbolicValue as SSV
+import chb.simulation.SimUtil as SU
+import chb.simulation.SimValue as SV
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.mips.MIPSDictionary import MIPSDictionary
+    from chb.mips.simulation.MIPSimulationState import MIPSimulationState
+
+
+@mipsregistry.register_tag("mfhc2", MIPSOpcode)
+class MIPSMoveWordFromHighHalfCoprocessor2(MIPSOpcode):
+    """MFHC2, rt, Impl, sel
+
+    Move Word From High Half of Coprocessor 2 Register.
+    Copy a word from the high half of a COP2 general register to a GPR.
+
+    args[0]: index of rt in mips dictionary
+    args[1]: Impl
+    args[2]: selector
+    """
+
+    def __init__(
+            self,
+            mipsd: "MIPSDictionary",
+            ixval: IndexedTableValue) -> None:
+        MIPSOpcode.__init__(self, mipsd, ixval)
+
+    @property
+    def operands(self) -> Sequence[MIPSOperand]:
+        return [self.mipsd.mips_operand(self.args[0])]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        """data format a:v
+
+        vars[0]: lhs
+        """
+
+        lhs = str(xdata.vars[0])
+        return lhs + ' := word from high half coprocessor 2'
