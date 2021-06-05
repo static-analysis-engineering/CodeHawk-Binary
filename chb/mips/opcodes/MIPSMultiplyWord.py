1a54d9b5c53113bb88c838e1878d88e69b510881
Henny Sipma <hennysipma@mac.com>
2021-06-05 15:46:08 -0700

    mips: move opcodes out

---
+++ chb/mips/opcodes/MIPSMultiplyWord.py
@@ -0,0 +1,146 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020-2021 Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import cast, List, Sequence, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.invariants.XXpr import XXpr
+
+from chb.mips.MIPSDictionaryRecord import mipsregistry
+from chb.mips.MIPSOpcode import MIPSOpcode, simplify_result
+from chb.mips.MIPSOperand import MIPSOperand
+
+import chb.simulation.SimSymbolicValue as SSV
+import chb.simulation.SimUtil as SU
+import chb.simulation.SimValue as SV
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.mips.MIPSDictionary import MIPSDictionary
+    from chb.mips.simulation.MIPSimulationState import MIPSimulationState
+
+
+@mipsregistry.register_tag("mult", MIPSOpcode)
+class MIPSMultiplyWord(MIPSOpcode):
+    """MULT rs, rt
+
+    Multiply Word.
+    Multiply 32-bit signed integers.
+
+    args[0]: index of hi in mips dictionary
+    args[1]: index of lo in mips dictionary
+    args[2]: index of rs in mips dictionary
+    args[3]: index of rt in mips dictionary
+    """
+
+    def __init__(
+            self,
+            mipsd: "MIPSDictionary",
+            ixval: IndexedTableValue) -> None:
+        MIPSOpcode.__init__(self, mipsd, ixval)
+
+    @property
+    def operands(self) -> Sequence[MIPSOperand]:
+        return [self.mipsd.mips_operand(i) for i in self.args]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        """data format vvxxxx
+
+        vars[0]: lhs-hi
+        vars[1]: lhs-lo
+        xprs[0]: rhs1 (rs)
+        xprs[1]: rhs2 (rt)
+        xprs[3]: rhs1 * rhs2 (syntactic)
+        xprs[4]: rhs1 * rhs2 (simplified)
+        """
+
+        hi = str(xdata.vars[0])
+        lo = str(xdata.vars[1])
+        result = xdata.xprs[2]
+        rresult = xdata.xprs[3]
+        xresult = simplify_result(xdata.args[4], xdata.args[5], result, rresult)
+        return '(' + hi + ',' + lo + ') := ' + xresult
+
+    @property
+    def dstlo_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[1])
+
+    @property
+    def dsthi_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[0])
+
+    @property
+    def src1_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[2])
+
+    @property
+    def src2_operand(self) -> MIPSOperand:
+        return self.mipsd.mips_operand(self.args[3])
+
+    # --------------------------------------------------------------------------
+    # Operation:
+    #    prod = GPR[rs][31..0] . GPR[rt][31..0]
+    #    lo <- prod[31..0]
+    #    hi <- prod[63..32]
+    # --------------------------------------------------------------------------
+    def simulate(self, iaddr: str, simstate: "MIPSimulationState") -> str:
+        dsthi = self.dsthi_operand
+        dstlo = self.dstlo_operand
+        src1op = self.src1_operand
+        src2op = self.src2_operand
+        src1val = simstate.get_rhs(iaddr, src1op)
+        src2val = simstate.get_rhs(iaddr, src2op)
+        if (
+                src1val.is_defined
+                and src1val.is_literal
+                and src2val.is_defined
+                and src2val.is_literal):
+            src1val = cast(SV.SimLiteralValue, src1val)
+            src2val = cast(SV.SimLiteralValue, src2val)
+            p = src1val.value * src2val.value
+            loval = p % (SU.max32 + 1)
+            hival = p >> 32
+            lhslo = simstate.set(iaddr, dstlo, SV.mk_simvalue(loval))
+            lhshi = simstate.set(iaddr, dsthi, SV.mk_simvalue(hival))
+            simstate.increment_program_counter()
+            return SU.simassign(
+                iaddr,
+                simstate,
+                lhslo,
+                SV.mk_simvalue(loval),
+                intermediates=str(lhshi) + ' := ' + str(hival))
+        else:
+            lhslo = simstate.set(iaddr, dstlo, SV.simUndefinedDW)
+            lhshi = simstate.set(iaddr, dsthi, SV.simUndefinedDW)
+            simstate.increment_program_counter()
+            return SU.simassign(iaddr, simstate, lhslo, SV.simUndefinedDW)
