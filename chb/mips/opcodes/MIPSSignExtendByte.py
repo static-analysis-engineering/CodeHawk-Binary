e8c73197ea64c3a5386933caef76580d94e13cab
Henny Sipma <hennysipma@mac.com>
2021-06-06 21:50:51 -0700

    mips: move opcodes out

---
+++ chb/mips/opcodes/MIPSSignExtendByte.py
@@ -0,0 +1,86 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2020 Kestrel Technology LLC
+# Copyright (c) 2020-2021 Henny Sipma
+# Copyright (c) 2021      Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import cast, List, Sequence, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.invariants.XXpr import XXpr
+
+from chb.mips.MIPSDictionaryRecord import mipsregistry
+from chb.mips.MIPSOpcode import MIPSOpcode, simplify_result
+from chb.mips.MIPSOperand import MIPSOperand
+
+import chb.simulation.SimSymbolicValue as SSV
+import chb.simulation.SimUtil as SU
+import chb.simulation.SimValue as SV
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.mips.MIPSDictionary import MIPSDictionary
+    from chb.mips.simulation.MIPSimulationState import MIPSimulationState
+
+
+@mipsregistry.register_tag("seb", MIPSOpcode)
+class MIPSSignExtendByte(MIPSOpcode):
+    """SEB rd, rt
+
+    Sign-Extend Byte.
+    Sign-extend the least significant byte of rt and store the value into rd.
+
+    args[0]: index of rd in mips dictionary
+    args[1]: index of rt in mips dictionary
+    """
+
+    def __init__(
+            self,
+            mipsd: "MIPSDictionary",
+            ixval: IndexedTableValue) -> None:
+        MIPSOpcode.__init__(self, mipsd, ixval)
+
+    @property
+    def operands(self) -> Sequence[MIPSOperand]:
+        return [self.mipsd.mips_operand(i) for i in self.args]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        """data format a:vxx
+
+        vars[0]: lhs
+        xprs[0]: rhs
+        xprs[1]: rhs (simplified)
+        """
+
+        lhs = str(xdata.vars[0])
+        rhs = xdata.xprs[0]
+        rrhs = xdata.xprs[1]
+        xrhs = simplify_result(xdata.args[1], xdata.args[2], rhs, rrhs)
+        return lhs + ' := ' + xrhs
