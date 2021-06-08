f488f1a6267e39a5dc8b33ec3ce3b438439b37a7
Henny Sipma <hennysipma@mac.com>
2021-06-07 17:16:19 -0700

    graphics: add typing

--- chb/graphics/DotCfg.py
+++ chb/graphics/DotCfg.py
@@ -29,7 +29,7 @@
 
 import chb.util.graphutil as UG
 
-from typing import Dict, List, Mapping, Set, Tuple, TYPE_CHECKING
+from typing import Any, Dict, List, Mapping, Optional, Set, Tuple, TYPE_CHECKING
 
 from chb.util.DotGraph import DotGraph
 
@@ -50,8 +50,9 @@ class DotCfg:
             showcalls: bool = False,        # show call instrs on nodes
             mips: bool = False,     # for mips subtract 4 from block end addr
             sink: str = None,      # restrict paths to basic block destination
-            segments: List[str] = [],    # restrict paths to include these basic blocks
-            replacements: Dict[str, str] = {}) -> None: # replacement text for node and edge labels
+            segments: List[str] = [],  # restrict paths to include these basic blocks
+            # replacement text for node and edge labels
+            replacements: Dict[str, str] = {}) -> None:
         self.fn = fn
         self.graphname = graphname
         self.looplevelcolors = looplevelcolors
@@ -65,7 +66,7 @@ class DotCfg:
         self.dotgraph = DotGraph(graphname)
 
     def build(self) -> DotGraph:
-        if not self.sink is None:
+        if self.sink is not None:
             self.restrict_nodes(self.sink)
         elif len(self.segments) > 0:
             self.restrict_paths(self.segments)
@@ -80,32 +81,32 @@ class DotCfg:
     def restrict_nodes(self, sink: str) -> None:
         nodes = self.fn.cfg.blocks
         edges = self.fn.cfg.edges   # adjacency list n -> [ n ]
-        if not sink in nodes:
+        if sink not in nodes:
             print('Sink ' + sink + ' not found in nodes')
             self.pathnodes = set(nodes.keys())
             return
-        g = UG.DirectedGraph(nodes,edges)
-        g.find_paths(self.fn.faddr,sink)
+        g = UG.DirectedGraph(list(nodes.keys()), edges)
+        g.find_paths(self.fn.faddr, sink)
         for p in g.paths:
-            print('Path: ' +  str(p))
+            print('Path: ' + str(p))
             self.pathnodes = self.pathnodes.union(p)
         if len(self.pathnodes) == 0:
             self.pathnodes = set(nodes.keys())
 
-    def restrict_paths(self,segments) -> None:
+    def restrict_paths(self, segments: List[str]) -> None:
         nodes = self.fn.cfg.blocks
         edges = self.fn.cfg.edges
         for b in segments:
-            if not b in nodes:
+            if b not in list(nodes.keys()):
                 print('Segment ' + b + ' not found in nodes')
                 self.pathnodes = set(nodes.keys())
                 return
-        segments = [ self.fn.faddr ] + segments
-        g  = UG.DirectedGraph(nodes,edges)
+        segments = [self.fn.faddr] + segments
+        g = UG.DirectedGraph(list(nodes.keys()), edges)
         for i in range(len(segments) - 1):
             src = segments[i]
             dst = segments[i+1]
-            g.find_paths(src,dst)
+            g.find_paths(src, dst)
         for p in g.paths:
             print('Path: ' + str(p))
             self.pathnodes = self.pathnodes.union(p)
@@ -126,27 +127,30 @@ class DotCfg:
         else:
             instraddr_i = int(instraddr, 16)
             if self.mips:
-                instraddr_i -= 4  #  take into account delay slot
+                instraddr_i -= 4  # take into account delay slot
             instraddr = hex(instraddr_i)
-        return self.fn.get_instruction(instraddr)
+        return self.fn.instruction(instraddr)
 
-    def to_json(self):
-        d  = {}
+    def to_json(self) -> Dict[str, Any]:
+        d: Dict[str, Any] = {}
         d['nodes'] = []
         d['edges'] = {}
         for n in self.fn.cfg.blocks:
             d['nodes'].append(str(n))
+
         for e in self.fn.cfg.edges:
             d['edges'][str(e)] = {}
-            def default():
+
+            def default() -> None:
                 for tgt in self.fn.cfg.edges[e]:
                     d['edges'][str(e)][str(tgt)] = 'none'
+
             if len(self.fn.cfg.edges[e]) > 1:
                 branchinstr = self.get_branch_instruction(e)
-                if branchinstr.is_branch_instruction():
-                    ftconditions = branchinstr.get_ft_conditions()
+                if branchinstr.is_branch_instruction:
+                    ftconditions = branchinstr.ft_conditions
                     if len(ftconditions) > 1:
-                        for i,tgt in enumerate(self.fn.cfg.edges[e]):
+                        for i, tgt in enumerate(self.fn.cfg.edges[e]):
                             d['edges'][str(e)][str(tgt)] = ftconditions[i]
                     else:
                         default()
@@ -156,28 +160,29 @@ class DotCfg:
                 default()
         return d
 
-    def replace_text(self,txt):
+    def replace_text(self, txt: str) -> str:
         result = txt
-        for src in sorted(self.replacements,key=lambda x:len(x),reverse=True):
-            result = result.replace(src,self.replacements[src])
+        for src in sorted(self.replacements, key=lambda x: len(x), reverse=True):
+            result = result.replace(src, self.replacements[src])
         return result
 
     def add_cfg_node(self, n: str) -> None:
-        if not n in self.pathnodes:
+        if n not in self.pathnodes:
             return
-        basicblock = self.fn.get_block(str(n))
+        basicblock = self.fn.block(str(n))
         blocktxt = str(n)
         color = 'lightblue'
         if self.showcalls:
             callinstrs = basicblock.call_instructions
-            pcallinstrs = [ str(i.annotation) for i in callinstrs ]
-            print(' \n'.join( [ str(a) for a in pcallinstrs ]))
+            pcallinstrs = [str(i.annotation) for i in callinstrs]
+            print(' \n'.join([str(a) for a in pcallinstrs]))
             if len(callinstrs) > 0:
-                blocktxt = (blocktxt
-                                + '\\n'
-                                + '\\n'.join([str(a) for a in callinstrs]))
+                blocktxt = (
+                    blocktxt
+                    + '\\n'
+                    + '\\n'.join([str(a) for a in callinstrs]))
         if len(self.looplevelcolors) > 0:
-            looplevels = self.fn.cfg.get_loop_levels(n)
+            looplevels = self.fn.cfg.loop_levels(n)
             if len(looplevels) > 0:
                 level = len(looplevels)
                 if level > len(self.looplevelcolors):
@@ -187,27 +192,30 @@ class DotCfg:
         if n == self.fn.faddr:
             color = 'purple'
         blocktxt = self.replace_text(blocktxt)
-        self.dotgraph.add_node(str(n),labeltxt=str(blocktxt),color=color)
+        self.dotgraph.add_node(str(n), labeltxt=str(blocktxt), color=color)
 
-    def add_cfg_edge(self,e):
-        if not e in self.pathnodes:
+    def add_cfg_edge(self, e: str) -> None:
+        if e not in self.pathnodes:
             return
-        def default():
+
+        def default() -> None:
             for tgt in self.fn.cfg.edges[e]:
                 if tgt in self.pathnodes:
-                    self.dotgraph.add_edge(str(e),str(tgt),labeltxt=None)
-        labeltxt = None
+                    self.dotgraph.add_edge(str(e), str(tgt), labeltxt=None)
+
+        labeltxt: Optional[str] = None
         if len(self.fn.cfg.edges[e]) > 1:
             if self.showpredicates:
                 branchinstr = self.get_branch_instruction(e)
-                if branchinstr and branchinstr.is_branch_instruction():
-                    ftconditions = branchinstr.get_ft_conditions()
+                if branchinstr and branchinstr.is_branch_instruction:
+                    ftconditions = branchinstr.ft_conditions
                     if len(ftconditions) == 2:
-                        for i,tgt in enumerate(self.fn.cfg.edges[e]):
+                        for i, tgt in enumerate(self.fn.cfg.edges[e]):
                             if tgt in self.pathnodes:
                                 labeltxt = str(ftconditions[i])
                                 labeltxt = self.replace_text(labeltxt)
-                                self.dotgraph.add_edge(str(e),str(tgt),labeltxt=labeltxt)
+                                self.dotgraph.add_edge(
+                                    str(e), str(tgt), labeltxt=labeltxt)
                     else:
                         default()
                 else:
@@ -216,10 +224,3 @@ class DotCfg:
                 default()
         else:
             default()
-        
-            
-            
-                        
-            
-            
-            
