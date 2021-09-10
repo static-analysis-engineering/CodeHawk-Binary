b270984e0a7d1a2870ae74f3b66f37a72085d1f0
Henny Sipma <hennysipma@mac.com>
2021-09-10 15:45:15 -0700

    ARM: add some floating point instructions

--- chb/arm/opcodes/ARMVCompare.py
+++ chb/arm/opcodes/ARMVCompare.py
@@ -25,7 +25,7 @@
 # SOFTWARE.
 # ------------------------------------------------------------------------------
 
-from typing import Any, Dict, List, Sequence, TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 
 from chb.app.InstrXData import InstrXData
 
@@ -33,23 +33,26 @@ from chb.arm.ARMDictionaryRecord import armregistry
 from chb.arm.ARMOpcode import ARMOpcode, simplify_result
 from chb.arm.ARMOperand import ARMOperand
 
-from chb.invariants.XXpr import XXpr
-
 import chb.util.fileutil as UF
 
 from chb.util.IndexedTable import IndexedTableValue
 
 if TYPE_CHECKING:
-    from chb.api.CallTarget import CallTarget
     from chb.arm.ARMDictionary import ARMDictionary
 
 
-@armregistry.register_tag("BL", ARMOpcode)
-class ARMBranchLink(ARMOpcode):
-    """Calls a subroutine at a PC-relative address.
+@armregistry.register_tag("VCMPE.F64", ARMOpcode)
+class ARMVCompare(ARMOpcode):
+    """Compares two floating-point numbers.
+
+    VCMP{E}<c>.F64 <Dd> <Dm>
+    VCMP{E}<c>.F32 <Sd> <Sm>
 
     tags[1]: <c>
-    args[0]: index of target operand in armdictionary
+    tags[2]: <Td>    data type (F32 or F64)
+    args[0]: nan (1 = raise Invalid Operation when one of the operands is NaN)
+    args[1]: index of d in armdictionary
+    args[2]: index of m in armdictionary
     """
 
     def __init__(
@@ -57,53 +60,22 @@ class ARMBranchLink(ARMOpcode):
             d: "ARMDictionary",
             ixval: IndexedTableValue) -> None:
         ARMOpcode.__init__(self, d, ixval)
-        self.check_key(2, 1, "BranchLink")
+        self.check_key(3, 3, "VCompare")
 
     @property
     def operands(self) -> List[ARMOperand]:
-        return [self.armd.arm_operand(self.args[0])]
-
-    def has_string_arguments(self, xdata: InstrXData) -> bool:
-        return any([x.is_string_reference for x in self.arguments(xdata)])
-
-    def has_stack_arguments(self, xdata: InstrXData) -> bool:
-        return any([x.is_stack_address for x in self.arguments(xdata)])
-
-    def annotated_call_arguments(
-            self, xdata: InstrXData) -> Sequence[Dict[str, Any]]:
-        return [x.to_annotated_value() for x in xdata.xprs]
-
-    def arguments(self, xdata: InstrXData) -> Sequence[XXpr]:
-        return xdata.xprs
-
-    def is_call(self, xdata: InstrXData) -> bool:
-        return len(xdata.tags) == 2 and xdata.tags[1] == "call"
-
-    def is_call_instruction(self, xdata: InstrXData) -> bool:
-        return xdata.has_call_target()
+        return [self.armd.arm_operand(self.args[i]) for i in [1, 2]]
 
     def annotation(self, xdata: InstrXData) -> str:
-        """data formats: call, jumptable, indirect jump
+        """xdata format: a:vxxxx.
 
-        call: [a:..., 'call'], <args> + <index of call-target in ixd>
-
-        or
-
-        xdata format: a:x .
-
-        xprs[0]: target operand
+        xprs[0]: d
+        xprs[1]: m
+        xprs[2]: d (rewritten)
+        xprs[3]: m (rewritten)
         """
 
-        if self.is_call(xdata) and xdata.has_call_target():
-            tgt = xdata.call_target(self.ixd)
-            args = ", ".join(str(x) for x in self.arguments(xdata))
-            return "call " + str(tgt) + "(" + args + ")"
-
-        ctgt = str(xdata.xprs[0])
-        return "call " + ctgt
-
-    def call_target(self, xdata: InstrXData) -> "CallTarget":
-        if self.is_call(xdata):
-            return xdata.call_target(self.ixd)
-        else:
-            raise UF.CHBError("Instruction is not a call: " + str(self))
+        rhs1 = str(xdata.xprs[2])
+        rhs2 = str(xdata.xprs[3])
+        comparison = "compare " + rhs1 + " and " + rhs2
+        return comparison
