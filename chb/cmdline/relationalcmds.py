18830d3cb2d642f31665ebbbc0b3b278149ed4a8
Henny Sipma <hennysipma@mac.com>
2021-09-10 15:39:13 -0700

    CLI: add commands for relational analysis

---
+++ chb/cmdline/relationalcmds.py
@@ -0,0 +1,78 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Aarno Labs, LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+"""Initiate relational analysis of two binaries."""
+
+import argparse
+import importlib
+import json
+
+from typing import NoReturn
+
+import chb.cmdline.commandutil as UC
+import chb.cmdline.XInfo as XI
+
+from chb.relational.RelationalAnalysis import RelationalAnalysis
+
+import chb.util.fileutil as UF
+
+
+def relational_analysis_cmd(args: argparse.Namespace) -> NoReturn:
+
+    # arguments
+    xname1: str = args.xname1
+    xname2: str = args.xname2
+    showfunctions: bool = args.functions
+    showinstructions: bool = args.instructions
+
+    try:
+        (path1, xfile1) = UC.get_path_filename(xname1)
+        UF.check_analysis_results(path1, xfile1)
+    except UF.CHBError as e:
+        print(str(e.wrap()))
+        exit(1)
+
+    xinfo1 = XI.XInfo()
+    xinfo1.load(path1, xfile1)
+
+    try:
+        (path2, xfile2) = UC.get_path_filename(xname2)
+        UF.check_analysis_results(path2, xfile2)
+    except UF.CHBError as e:
+        print(str(e.wrap()))
+        exit(1)
+
+    xinfo2 = XI.XInfo()
+    xinfo2.load(path2, xfile2)
+
+    app1 = UC.get_app(path1, xfile1, xinfo1)
+    app2 = UC.get_app(path2, xfile2, xinfo2)
+
+    relanalysis = RelationalAnalysis(app1, app2)
+
+    print(relanalysis.report(showfunctions, showinstructions))
+
+    exit(0)
