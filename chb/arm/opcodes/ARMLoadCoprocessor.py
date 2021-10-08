500e1c8d485b6c2f9d531c753e8a0aa792112dbb
Henny Sipma <hennysipma@mac.com>
2021-10-08 16:21:54 -0700

    ARM: add new instructions

---
+++ chb/arm/opcodes/ARMLoadCoprocessor.py
@@ -0,0 +1,71 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import List, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.arm.ARMDictionaryRecord import armregistry
+from chb.arm.ARMOpcode import ARMOpcode, simplify_result
+from chb.arm.ARMOperand import ARMOperand
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.arm.ARMDictionary import ARMDictionary
+
+
+@armregistry.register_tag("LDCL", ARMOpcode)
+@armregistry.register_tag("LDC", ARMOpcode)
+class ARMLoadCoprocessor(ARMOpcode):
+    """Loads memory data from a sequence of addresses.
+
+    LDC{L}<c> <coproc>, <CRd>, [<Rn>, +/-#<imm>]
+
+    tags[1]: <c>
+    args[0]: is-long
+    args[1]: coproc
+    args[2]: CRd
+    args[3]: index of memory address
+    args[4]: optional option
+    """
+
+    def __init__(
+            self,
+            d: "ARMDictionary",
+            ixval: IndexedTableValue) -> None:
+        ARMOpcode.__init__(self, d, ixval)
+        self.check_key(2, 5, "LoadCoprocessor")
+
+    @property
+    def operands(self) -> List[ARMOperand]:
+        return [self.armd.arm_operand(self.args[3])]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        return "load coprocessor"
