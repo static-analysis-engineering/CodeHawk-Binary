b5ca25ebc06faa79128b3facd22580857498d5ae
Henny Sipma <hennysipma@mac.com>
2022-05-16 21:55:47 -0700

    CHKX: add command to extract call records

--- chb/cmdline/reportcmds.py
+++ chb/cmdline/reportcmds.py
@@ -28,19 +28,141 @@
 """Support functions for the command-line interpreter."""
 
 import argparse
+import json
 
-from typing import Dict, List, NoReturn, Tuple
+from typing import (
+    Any, Callable, cast, Dict, List, NoReturn, Optional, Sequence, Tuple)
+
+from chb.app.Instruction import Instruction
 
 import chb.cmdline.commandutil as UC
 import chb.cmdline.XInfo as XI
 
+from chb.invariants.XXpr import XprCompound, XprConstant, XXpr
+
 import chb.util.fileutil as UF
+
+
+def dst_stack_arg_offset(instr: Instruction, dstargix: int) -> Optional[int]:
+    try:
+        dstarg = instr.call_arguments[dstargix]
+        if dstarg.is_stack_address:
+            dstarg = cast(XprCompound, dstarg)
+            return (-dstarg.stack_address_offset())
+        else:
+            print("Not a stack address: " + str(dstarg))
+            return None
+    except Exception as e:
+        print("Exception in " + str(instr) + ": " + str(e))
+        return None
+
+
+destination_arguments: Dict[str, int] = {
+    "memcpy": 0,
+    "snprintf": 0,
+    "sprintf": 0,
+    "strlcpy": 0,
+    "strcat": 0,
+    "strcpy": 0,
+    "strlcpy": 0,
+    "strncpy": 0
+}
+
+
+class CallRecord:
+
+    def __init__(
+            self,
+            faddr: str,
+            instr: Instruction,
+            fname: Optional[str] = None) -> None:
+        self._faddr = faddr
+        self._fname = fname
+        self._instr = instr
+
+    @property
+    def instr(self) -> Instruction:
+        return self._instr
+
+    @property
+    def faddr(self) -> str:
+        return self._faddr
+
+    @property
+    def iaddr(self) -> str:
+        return self.instr.iaddr
+
+    def has_fname(self) -> bool:
+        return self._fname is not None
+
+    @property
+    def fname(self) -> str:
+        if self._fname is not None:
+            return self._fname
+        else:
+            raise UF.CHBError("Function does not have a name")
+
+    @property
+    def callee(self) -> str:
+        return str(self.instr.call_target)
+
+    @property
+    def arguments(self) -> Sequence["XXpr"]:
+        return self.instr.call_arguments
+
+    @property
+    def write_destination(self) -> Optional[str]:
+        if self.callee in destination_arguments:
+            argix = destination_arguments[self.callee]
+            if len(self.arguments) > argix:
+                dstarg = self.arguments[destination_arguments[self.callee]]
+            else:
+                print("Argument " + str(argix) + " not present for " + self.callee)
+                return None
+            if dstarg.is_stack_address:
+                return "stack"
+            elif dstarg.is_heap_address:
+                return "heap"
+            elif dstarg.is_global_address:
+                return "global"
+            else:
+                return None
+        else:
+            return None
+
+    def dest_stack_frame_size(self) -> Optional[int]:
+        dest = self.write_destination
+        if dest and dest == "stack":
+            return dst_stack_arg_offset(
+                self.instr, destination_arguments[self.callee])
+        else:
+            return None
+
+    def to_dict(self) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+        writedst = self.write_destination
+        result["faddr"] = self.faddr
+        result["iaddr"] = self.iaddr
+        if self.has_fname():
+            result["fname"] = self.fname
+        result["callee"] = self.callee
+        result["arguments"] = [str(a) for a in self.arguments]
+        if writedst is not None:
+            result["writes-to"] = writedst
+            if writedst == "stack":
+                framesize = self.dest_stack_frame_size()
+                if framesize is not None:
+                    result["dst-framesize"] = framesize
+        return result
 
 
 def report_calls(args: argparse.Namespace) -> NoReturn:
 
     # arguments
     xname: str = args.xname
+    outputfile: str = args.outputfile
+    verbose: bool = args.verbose
+    callees: List[str] = args.callees
 
     try:
         (path, xfile) = UC.get_path_filename(xname)
@@ -55,20 +177,51 @@ def report_calls(args: argparse.Namespace) -> NoReturn:
     app = UC.get_app(path, xfile, xinfo)
 
     calls = app.call_instructions()
-    calllist: List[Tuple[str, str]] = []
+    calllist: List[CallRecord] = []
 
     for faddr in sorted(calls):
-        print("\nFunction " + faddr)
+        fname = (
+            app.function_name(faddr)
+            if app.has_function_name(faddr)
+            else None)
+        if verbose:
+            print("\nFunction " + faddr)
         for baddr in calls[faddr]:
-            print("  Block " + baddr)
             for instr in calls[faddr][baddr]:
-                calllist.append((instr.annotation, faddr))
-                print("     " + str(instr))
+                calltgt = str(instr.call_target)
+                if "all" in callees or calltgt in callees:
+                    callrec = CallRecord(faddr, instr, fname=fname)
+                    calllist.append(callrec)
+                    if verbose:
+                        print("     " + str(instr))
 
-    print("\nCalls aggregated")
-    print("------------------")
-    for (c, faddr) in sorted(calllist):
-        print(faddr.ljust(12) + c)
+    results: Dict[str, Any] = {}
+    results["application"] = {}
+    results["application"]["name"] = xfile
+    results["application"]["md5"] = xinfo.md5
+    results["calls"] = [callrec.to_dict() for callrec in calllist]
+
+    filename = outputfile + ".json"
+    with open(filename, "w") as fp:
+        json.dump(results, fp, indent=2)
+
+    stats: Dict[str, Dict[str, int]] = {}
+
+    for c in calllist:
+        callee = c.callee
+        stats.setdefault(callee, {})
+        dst = c.write_destination
+        if dst is None:
+            dst = "unknown"
+        stats[callee].setdefault(dst, 0)
+        stats[callee][dst] += 1
+
+    print("Statistics")
+    print("==========")
+    for (call, counts) in stats.items():
+        print(call + " (" + str(sum([counts[n] for n in counts])) + ")")
+        for (d, n) in counts.items():
+            print("  " + d.ljust(8) + str(n).rjust(5))
 
     exit(0)
 
