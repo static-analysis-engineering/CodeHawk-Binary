80350d3f66bc42f32a1db4058997a7cd17dc03f7
Henny Sipma <hennysipma@mac.com>
2022-05-25 16:26:06 -0700

    AST: add documentation for serialization format

--- chb/ast/ASTSerializer.py
+++ chb/ast/ASTSerializer.py
@@ -24,7 +24,251 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # ------------------------------------------------------------------------------
-"""AST serialization to json format."""
+"""AST serialization to json format.
+
+Serialization formats:
+======================
+
+Statements
+----------
+
+- return_stmt:
+  tag: "return"
+  args:
+     0: stmtid
+     1: return expr, or -1 if returning void
+
+- block-stmt:
+  tag: "block"
+  args:
+    0: stmtid
+    1..: id's of statements contained in the block
+
+- branch-stmt:
+  tag: "if"
+  pc-offset: integer that gives the distance between this statement and the
+             target statement
+  args:
+    0: stmtid
+    1: id of condition expr
+    2: id of then-branch statement
+    3: id of else-branch statement
+
+- instruction-sequence:
+  tag: "instrs"
+  args:
+    0: stmtid
+    1..: id's of instructions contained in the statement
+
+
+Instructions
+------------
+
+- call-instr:
+  tag: "call"
+  args:
+    0: instrid
+    1: id of left-hand-side (lval) or -1 if there is no left-hand-side
+    2: id of expression that represents the target of the call
+    3..: id's of the argument expressions
+
+- assign-instr
+  tag: "assign"
+  args:
+    0: instrid
+    1: id of left-hand-side (lval)
+    2: id of right-hand-side (expr)
+
+
+Lval-related nodes
+------------------
+
+- varinfo:
+  tag: "varinfo"
+  name: name of the variable
+  (optional)descr: description of the variable, free-form string
+  args:
+    0: id of the type of the variable of -1 if not known
+    1: index of parameter (zero-based) if this is a formal or -1 otherwise
+    2: global address (integer) if this variable is a global and its address
+       is known, -1 otherwise
+
+- variable
+  tag: "var"
+  name: name of the variable
+  args:
+    0: id of the varinfo of this variable
+
+- memory-reference
+  tag: "memref"
+  args:
+    0: id of the expression that gets dereferenced
+
+- no-offset
+  tag: "no-offset"
+  args: -
+
+- field-offset
+  tag: "field-offset"
+  fname: name of the field (string)
+  args:
+    0: compinfo compkey
+    1: id of sub-offset
+
+- index-offset
+  tag: "index-offset"
+  args:
+    0: id of index expression
+    1: id of sub-offset
+
+
+Expressions
+-----------
+
+- integer-constant
+  tag: "integer-constant"
+  value: integer value as a string
+  args: -
+
+- global-address
+  tag: "global-address"
+  value: integer value as a string
+  args:
+     0: id of the expression that forms the address (lval-expr)
+
+- string-constant
+  tag: "string-constant"
+  cstr: the string
+  (optional)va: the address of the string (as a hex-string)
+  args:
+    0: id of expression that forms the string address, or -1 if not known
+
+- lval-expression
+  tag: "lval-expr"
+  args:
+    0: id of lval
+
+- substituted-expression
+  tag: "substituted-expression"
+  assigned: instrid of the assign instruction that provides the expression
+  args:
+    0: id of the lval of the original lval-expression
+    1: id of the substituted expression
+
+- size-of-type-expression
+  tag: "size-of"
+  args:
+    0: id of the type of which the size is taken
+
+- cast-expression
+  tag: "cast-expr"
+  args:
+    0: id of the target type to which is cast
+    1: id of the expression being cast
+
+- unary-expression
+  tag: "unary-op"
+  op: name of operator ("neg", or "bnot", or "lnot")
+  args:
+    0: id of subexpression
+
+- binary-expression
+  tag: "binary-op"
+  op: name of operator ("plus", "minus", "mult", "div", "mod", "lsl", "lsr",
+        "asr", "lt", "gt", "le", "ge", "eq", "ne", "band", "bxor", "bor",
+        "land", "lor")
+  args:
+    0: id of first subexpression
+    1: id of second subexpression
+
+- question-expression
+  tag: "question"
+  args:
+    0: id of first subexpression (conditional)
+    1: id of second subexpression
+    2: id of third subexpression
+
+
+Types
+-----
+
+- void-type:
+  tag: "void"
+  args: -
+
+- integer-type
+  tag: "int"
+  ikind: indication of the signedness/width of the integer
+         ("ichar", "ischar", "iuchar", "ibool", "iint", "iuint", "ishort",
+          "iushort", "ilong", "iulong", "ilonglong")
+  args: -
+
+- float-type
+  tag: "float"
+  fkind: indication of the precision ("float", "fdouble", "flongdouble")
+  args: -
+
+- pointer type
+  tag: "ptr"
+  args:
+    0: id of the target type of the pointer
+
+- array type
+  tag: "array"
+  args:
+    0: id of the element type
+    1: id of the expression that denotes the size or -1 if not available
+
+- function type
+  tag: "funtype"
+  args:
+    0: id of the return type
+    1: id of the function arguments (funargs), or -1 if not available
+    2: 1 if this function is a varargs function, 0 otherwise
+
+- function-arguments
+  tag: "funargs"
+  args:
+    0..: id's of the individual function-arguments (funarg)
+
+- function-argument
+  tag: "funarg"
+  name: name of the function argument (in the signature)
+  args:
+    0: id of the type of the function argument
+
+- named type (typedef)
+  tag: "typdef"
+  name: name given to the type
+  args:
+    0: id of the type assigned to the name
+
+- struct/union type
+  tag: "comptyp"
+  cname: name of the struct/union
+  args:
+    0: compkey
+
+- builtin-va-list
+  tag: "builtin-va-list"
+  args: -
+
+- fieldinfo (struct/union field)
+  tag: "fieldinfo"
+  name: name of the field
+  args:
+    0: id of the type of the field
+    1: compkey of the struct to which this field belongs
+    2: byteoffset of the field in the struct if available, -1 otherwise
+
+- compinfo (struct/union)
+  tag: "compinfo"
+  name: name of the struct/union
+  args:
+    0: 1 if this compinfo represents a union, 0 otherwise
+    1..: id's of fieldinfo's that make up the struct or union
+
+"""
 
 from typing import Any, cast, Dict, List, Tuple
 
@@ -73,6 +317,8 @@ class ASTSerializer(ASTIndexer):
         node: Dict[str, Any] = {"tag": stmt.tag}
         if stmt.has_return_value():
             args.append(stmt.expr.index(self))
+        else:
+            args.append(-1)
         return self.add(tags, args, node)
 
     def index_block_stmt(self, stmt: AST.ASTBlock) -> int:
@@ -185,6 +431,8 @@ class ASTSerializer(ASTIndexer):
         node: Dict[str, Any] = {"tag": expr.tag, "cstr": expr.cstr}
         if expr.address_expr is not None:
             args.append(expr.address_expr.index(self))
+        else:
+            args.append(-1)
         if expr.string_address is not None:
             tags.append(expr.string_address)
             node["va"] = expr.string_address
@@ -271,14 +519,16 @@ class ASTSerializer(ASTIndexer):
         node: Dict[str, Any] = {"tag": typ.tag}
         if typ.size_expr is not None:
             args.append(typ.size_expr.index(self))
+        else:
+            args.append(-1)
         return self.add(tags, args, node)
 
     def index_fun_typ(self, typ: AST.ASTTypFun) -> int:
         tags: List[str] = [typ.tag]
         args: List[int] = [typ.returntyp.index(self)]
         node: Dict[str, Any] = {"tag": typ.tag}
-        if typ.argtypes is not None:
-            args.append(typ.argtypes.index(self))
+        args.append(-1 if typ.argtypes is None else typ.argtypes.index(self))
+        args.append(1 if typ.is_varargs else 0)
         return self.add(tags, args, node)
 
     def index_funargs(self, funargs: AST.ASTFunArgs) -> int:
@@ -309,6 +559,7 @@ class ASTSerializer(ASTIndexer):
         tags: List[str] = [finfo.tag, finfo.fieldname]
         args: List[int] = [finfo.fieldtype.index(self), finfo.compkey]
         node: Dict[str, Any] = {"tag": finfo.tag, "name": finfo.fieldname}
+        args.append(-1 if finfo.byteoffset is None else finfo.byteoffset)
         return self.add(tags, args, node)
 
     def index_compinfo(self, cinfo: AST.ASTCompInfo) -> int:
