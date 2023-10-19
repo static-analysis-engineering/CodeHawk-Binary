adf8ec042721bf901a040183fb5534e7918b0131
Henny Sipma <hennysipma@mac.com>
2023-10-19 10:14:38 -0700

    ARM: new opcodes

---
+++ chb/arm/opcodes/ARMBreakpoint.py
@@ -0,0 +1,85 @@
+# ------------------------------------------------------------------------------
+# CodeHawk Binary Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2023  Aarno Labs LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+from typing import List, Tuple, TYPE_CHECKING
+
+from chb.app.InstrXData import InstrXData
+
+from chb.arm.ARMDictionaryRecord import armregistry
+from chb.arm.ARMOpcode import ARMOpcode, simplify_result
+from chb.arm.ARMOperand import ARMOperand
+
+import chb.ast.ASTNode as AST
+from chb.astinterface.ASTInterface import ASTInterface
+
+import chb.util.fileutil as UF
+
+from chb.util.IndexedTable import IndexedTableValue
+
+if TYPE_CHECKING:
+    from chb.arm.ARMDictionary import ARMDictionary
+
+
+@armregistry.register_tag("BKPT", ARMOpcode)
+class ARMBreakpoint(ARMOpcode):
+    """Breakpoint for debugger; operand is ignored by processor.
+
+    BKPT #<imm>
+
+    args[0]: imm
+    """
+
+    def __init__(self, d: "ARMDictionary", ixval: IndexedTableValue) -> None:
+        ARMOpcode.__init__(self, d, ixval)
+        self.check_key(1, 1, "Breakpoint")
+
+    @property
+    def operands(self) -> List[ARMOperand]:
+        return [self.armd.arm_operand(self.args[0])]
+
+    @property
+    def opargs(self) -> List[ARMOperand]:
+        return [self.armd.arm_operand(self.args[0])]
+
+    def annotation(self, xdata: InstrXData) -> str:
+        return "breakpoint"
+
+    def ast_prov(
+            self,
+            astree: ASTInterface,
+            iaddr: str,
+            bytestring: str,
+            xdata: InstrXData) -> Tuple[
+                List[AST.ASTInstruction], List[AST.ASTInstruction]]:
+
+        annotations: List[str] = [iaddr, "BKPT"]
+
+        nopinstr = astree.mk_nop_instruction(
+            "BKPT", iaddr=iaddr, bytestring=bytestring, annotations=annotations)
+        astree.add_instr_address(nopinstr, [iaddr])
+
+        return ([], [nopinstr])
