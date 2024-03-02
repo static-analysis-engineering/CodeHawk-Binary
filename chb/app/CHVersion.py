50bc4a1611d76e6572329a4a7dab61df2ead77ac
Henny Sipma <hennysipma@mac.com>
2024-03-01 21:11:16 -0800

    update version

--- chb/app/CHVersion.py
+++ chb/app/CHVersion.py
@@ -1,1 +1,1 @@
-chbversion: str = "0.3.0-20240301"
+chbversion: str = "0.3.0-20240301A"
--- chb/cmdline/astcmds.py
+++ chb/cmdline/astcmds.py
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
-# Copyright (c) 2022-2023  Aarno Labs, LLC
+# Copyright (c) 2022-2024  Aarno Labs, LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -121,7 +121,6 @@ def library_call_targets(app: AppAccess, faddrs: List[str]) -> List[str]:
 
     result: Set[str] = set([])
     for faddr in faddrs:
-        print("DEBUG:faddr: " + faddr)
         if app.has_function(faddr):
             f = app.function(faddr)
             fcallees = f.call_instructions()
