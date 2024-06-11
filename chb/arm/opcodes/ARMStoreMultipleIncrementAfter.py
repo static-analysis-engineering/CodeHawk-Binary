ed28136fbe7961bd6689efec3c9d421c5a74cef0
Ricardo Baratto <ricardo@aarno-labs.com>
2024-06-11 12:28:34 -0400

    Initial implementation of ast_prov for STMIA
    Copy/pasted and modified from STMDB. Includes writeback functionality,
    which simply means the final address is stored back into the
    register. Most of the code is copy/pasted from STMDB.
    
    Missing proper annotation, I wasn't 100% sure what should go in there,
    so I'm just marking it as TODO for Henny to fill in so we can make
    progress.

--- chb/arm/opcodes/ARMStoreMultipleIncrementAfter.py
+++ chb/arm/opcodes/ARMStoreMultipleIncrementAfter.py
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------------------------
 # The MIT License (MIT)
 #
-# Copyright (c) 2021-2023  Aarno Labs LLC
+# Copyright (c) 2021-2024  Aarno Labs LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@
 # SOFTWARE.
 # ------------------------------------------------------------------------------
 
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Tuple
 
 from chb.app.InstrXData import InstrXData
 
@@ -57,7 +57,21 @@ class ARMStoreMultipleIncrementAfter(ARMOpcode):
     args[1]: index of Rn in arm dictionary
     args[2]: index of registers in arm dictionary
     args[3]: index of base memory address
-    args[4]: thumb-wide
+
+    xdata:
+    ----------------------------------------
+    vars[0]: base
+    vars[1..n]: lhs memory locations where values are stored
+    xprs[0]: base
+    xprs[1]: updated base (may be unchanged in case of no writeback)
+    xprs[2]: updated base (simplified)
+    xprs[3..n+2]: values of registers being stored (simplified)
+    rdefs[0]: reaching definition base register
+    rdefs[1..n]: reaching definitions of registers being stored
+    uses[0]: use of base register
+    uses[1..n]: use of memory variables
+    useshigh[0]: use-high of base register
+    useshigh[1..n]: use-high of memory variables
     """
 
     def __init__(
@@ -88,6 +102,10 @@ class ARMStoreMultipleIncrementAfter(ARMOpcode):
             + ", "
             + str(self.armd.arm_operand(self.args[2])))
 
+    @property
+    def writeback(self) -> bool:
+        return self.args[0] == 1
+
     def is_store_instruction(self, xdata: InstrXData) -> bool:
         return True
 
@@ -98,6 +116,7 @@ class ARMStoreMultipleIncrementAfter(ARMOpcode):
         xprs[0..n]: rhs expressions
         """
 
+        # TODO: Consider self.writeback like in the load case
         return '; '.join(
             str(lhs) + " := " + str(x) for (lhs, x) in zip(xdata.vars, xdata.xprs))
 
@@ -112,60 +131,99 @@ class ARMStoreMultipleIncrementAfter(ARMOpcode):
     # if wback then
     #   R[n] = R[n] + 4 * BitCount(registers);
     # --------------------------------------------------------------------------
-    def assembly_ast(
+    def ast_prov(
             self,
             astree: ASTInterface,
             iaddr: str,
             bytestring: str,
-            xdata: InstrXData) -> List[AST.ASTInstruction]:
-        baseop = self.opargs[0]
-        regsop = self.opargs[1]
-        if not regsop.is_register_list:
-            raise UF.CHBError("Argument to STM is not a register list")
+            xdata: InstrXData) -> Tuple[
+                List[AST.ASTInstruction], List[AST.ASTInstruction]]:
 
-        (reglval, _, _) = baseop.ast_lvalue(astree)
-        (regrval, _, _) = baseop.ast_rvalue(astree)
+        regcount = len(xdata.reachingdefs) - 1
+        baselhs = xdata.vars[0]
+        memlhss = xdata.vars[1:]
+        baserhs = xdata.xprs[0]
+        baseresult = xdata.xprs[1]
+        baseresultr = xdata.xprs[2]
+        regrhss = xdata.xprs[3:3 + regcount]
+        #rregrhss = xdata.xprs[3 + regcount:3 + (2 * regcount)]
+        baserdef = xdata.reachingdefs[0]
+        regrdefs = xdata.reachingdefs[1:]
+        baseuses = xdata.defuses[0]
+        memuses = xdata.defuses[1:]
+        baseuseshigh = xdata.defuseshigh[0]
+        memuseshigh = xdata.defuseshigh[1:]
 
-        instrs: List[AST.ASTInstruction] = []
-        registers = regsop.registers
-        reg_incr = 4 * len(registers)
-        reg_offset = 0
-        for r in registers:
-            reg_offset_c = astree.mk_integer_constant(reg_offset)
-            addr = astree.mk_binary_op("plus", regrval, reg_offset_c)
-            lhs = astree.mk_memref_lval(addr)
-            rhs = astree.mk_register_variable_expr(r)
-            instrs.append(astree.mk_assign(lhs, rhs))
-            reg_offset += 4
-        if self.args[0] == 1:
-            reg_incr_c = astree.mk_integer_constant(reg_incr)
-            reg_rhs = astree.mk_binary_op("plus", regrval, reg_incr_c)
-            instrs.append(astree.mk_assign(
-                reglval, reg_rhs, iaddr=iaddr, bytestring=bytestring))
+        annotations: List[str] = [iaddr, "STMIA"]
 
-        return instrs
+        ll_wbackinstrs: List[AST.ASTInstruction] = []
+        hl_wbackinstrs: List[AST.ASTInstruction] = []
 
-    def ast(self,
-            astree: ASTInterface,
-            iaddr: str,
-            bytestring: str,
-            xdata: InstrXData) -> List[AST.ASTInstruction]:
-        vars = xdata.vars
-        xprs = xdata.xprs[:-1]
-        basexpr = xdata.xprs[-1]
+        (baselval, _, _) = self.opargs[0].ast_lvalue(astree)
+        (baserval, _, _) = self.opargs[0].ast_rvalue(astree)
 
-        instrs: List[AST.ASTInstruction] = []
+        # Writeback means that the final address that is stored to is written
+        # back into Rn
+        if self.writeback:
+            baseincr = astree.mk_integer_constant(4 * regcount)
+            ll_base_rhs = astree.mk_binary_op("plus", baserval, baseincr)
+            ll_base_assign = astree.mk_assign(baselval, ll_base_rhs, iaddr=iaddr)
+            ll_wbackinstrs.append(ll_base_assign)
 
-        for (v, x) in zip(vars, xprs):
-            lhss = XU.xvariable_to_ast_lvals(v, xdata, astree)
-            rhss = XU.xxpr_to_ast_exprs(x, xdata, iaddr, astree)
-            if len(lhss) == 1 and len(rhss) == 1:
-                lhs = lhss[0]
-                rhs = rhss[0]
-                instrs.append(astree.mk_assign(
-                    lhs, rhs, iaddr=iaddr, bytestring=bytestring))
-            else:
+            hl_base_lhss = XU.xvariable_to_ast_lvals(baselhs, xdata, astree)
+            hl_base_rhss = XU.xxpr_to_ast_exprs(baseresultr, xdata, iaddr, astree)
+            if len(hl_base_lhss) != 1 or len(hl_base_rhss) != 1:
                 raise UF.CHBError(
-                    "ARMStoreMulipleIncrementAfter: multiple expressions/lvals in ast")
+                    "StoreMultipleIncrementAfter (STMIA): error in wback assign")
+            hl_base_lhs = hl_base_lhss[0]
+            hl_base_rhs = hl_base_rhss[0]
+            hl_base_assign = astree.mk_assign(hl_base_lhs, hl_base_rhs, iaddr=iaddr)
+            hl_wbackinstrs.append(hl_base_assign)
 
-        return instrs
+            astree.add_instr_mapping(hl_base_assign, ll_base_assign)
+            astree.add_instr_address(hl_base_assign, [iaddr])
+            astree.add_expr_mapping(hl_base_rhs, ll_base_rhs)
+            astree.add_lval_mapping(hl_base_lhs, baselval)
+            astree.add_expr_reachingdefs(ll_base_rhs, [baserdef])
+            astree.add_lval_defuses(hl_base_lhs, baseuses)
+            astree.add_lval_defuses_high(hl_base_lhs, baseuseshigh)
+
+        def default() -> Tuple[List[AST.ASTInstruction], List[AST.ASTInstruction]]:
+            ll_instrs: List[AST.ASTInstruction] = []
+            hl_instrs: List[AST.ASTInstruction] = []
+            regsop = self.opargs[1]
+            registers = regsop.registers
+            base_incr = 0
+            base_offset = base_incr
+            for (i, r) in enumerate(registers):
+                base_offset_c = astree.mk_integer_constant(base_offset)
+                addr = astree.mk_binary_op("plus", baserval, base_offset_c)
+                ll_lhs = astree.mk_memref_lval(addr)
+                ll_rhs = astree.mk_register_variable_expr(r)
+                ll_assign = astree.mk_assign(ll_lhs, ll_rhs, iaddr=iaddr)
+                ll_instrs.append(ll_assign)
+                lhs = memlhss[i]
+                rhs = regrhss[i]
+                hl_lhss = XU.xvariable_to_ast_lvals(lhs, xdata, astree)
+                hl_rhss = XU.xxpr_to_ast_exprs(rhs, xdata, iaddr, astree)
+                if len(hl_lhss) != 1 or len(hl_rhss) != 1:
+                    raise UF.CHBError(
+                        "StoreMultipleIncrementAfter (STMIA): error in assigns")
+                hl_lhs = hl_lhss[0]
+                hl_rhs = hl_rhss[0]
+                hl_assign = astree.mk_assign(hl_lhs, hl_rhs, iaddr=iaddr)
+                hl_instrs.append(hl_assign)
+
+                astree.add_instr_mapping(hl_assign, ll_assign)
+                astree.add_instr_address(hl_assign, [iaddr])
+                astree.add_expr_mapping(hl_rhs, ll_rhs)
+                astree.add_lval_mapping(hl_lhs, ll_lhs)
+                astree.add_expr_reachingdefs(ll_rhs, [regrdefs[i]])
+                astree.add_lval_defuses(hl_lhs, memuses[i])
+                astree.add_lval_defuses_high(hl_lhs, memuseshigh[i])
+
+                base_offset += 4
+
+            return (hl_instrs + hl_wbackinstrs, ll_instrs + ll_wbackinstrs)
+
+        return default()
